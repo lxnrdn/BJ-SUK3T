ebbda45f5ceccad71a2b3db0dbd212f366d86ee6
xlolo_id <64420551+lxnrdn@users.noreply.github.com>
2024-11-08 22:51:58 +0700

    Update bj.py

--- tuyul/bj.py
+++ tuyul/bj.py
@@ -79,17 +79,17 @@ async def main():
     
     print(f"Jumlah akun: {len(user_ids)}")
     
-    with open('proxy.txt', 'r') as proxy_file:
-        proxy = proxy_file.read().splitlines()
+    with open('local_proxies.txt', 'r') as proxy_file:
+        local_proxies = proxy_file.read().splitlines()
     
     tasks = []
-    proxy_count = len(proxy)
+    proxy_count = len(local_proxies)
     user_count = len(user_ids)
     
     # Ensure each proxy is used at least once
     for i in range(max(proxy_count, user_count)):
         user_id = user_ids[i % user_count]
-        proxy = proxy[i % proxy_count]
+        proxy = local_proxies[i % proxy_count]
         tasks.append(asyncio.ensure_future(connect_to_wss(proxy, user_id)))
     
     await asyncio.gather(*tasks)
